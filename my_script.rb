fa4454ec673e94a1272f037a4285f2404b9d77b0
Mr_Slav <1@1.ru>
2015-06-12 00:47:49 +0300

    v1.0. User registration/Logout/Login

--- my_script.rb
+++ my_script.rb
@@ -1,3 +1,29 @@
+require 'selenium-webdriver'
 
-class 
+#registration
+def register_as(login)
+  @browser.find_element(:id, 'user_login').send_keys login
+  @browser.find_element(:id, 'user_password').send_keys 'Qwerty123'
+  @browser.find_element(:id, 'user_password_confirmation').send_keys 'Qwerty123'
+  @browser.find_element(:id, 'user_firstname').send_keys 'Johny'
+  @browser.find_element(:id, 'user_lastname').send_keys 'Gun'
+  @browser.find_element(:id, 'user_mail').send_keys(login + '@1.ru')
+  @browser.find_element(:name, 'commit').click
 end
+
+@browser = Selenium::WebDriver.for :firefox
+@browser.get 'http://demo.redmine.org'
+@browser.find_element(:class, 'register').click
+
+login= 'Johny'
+register_as(login)
+
+#logout/login
+@browser.find_element(:class, 'logout').click
+@browser.find_element(:class, 'login').click
+sleep 1
+@browser.find_element(:id, 'username').send_keys login
+@browser.find_element(:id, 'password').send_keys 'Qwerty123'
+@browser.find_element(:name, 'login').click
+
+@browser.quit
